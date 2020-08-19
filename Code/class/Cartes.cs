9db0b8bff2c5dc3a0aa159c18edfb94119e76bbe
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2020-08-19 16:37:55 +0300

    Class 19/08

---
+++ Code/class/Cartes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Cartes
+    {
+        public int Valeur { get; set; }
+        public string Nom { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Code/class/ClassJetons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker.Code
+{
+    public class ClassJetons
+    {
+        public int valeurJetons { get; set; }
+        public string imageJetons { get; set; }
+       
+    }
+}
