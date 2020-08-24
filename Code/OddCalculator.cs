6f881c55994712ce20b12b455e397b2bc718c588
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2020-08-24 16:08:58 +0200

    Started coding

---
+++ Code/OddCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker
+{
+    public partial class Play : Form
+    {
+        public enum Rank
+        {
+            Deux = 2, Trois = 3, Quatre = 4, Cinq = 5, Six = 6, Sept = 7, Huit = 8, Neuf = 9, Dix = 10, Valet =11, Reine = 12, Roi = 13, As = 14, 
+        }
+
+        public enum Symb
+        {
+            Coeurs,Trefles,Carreaux,Piques
+        }
+        public void CalcOdd()
+        {
+
+        }
+    }
+}
+
